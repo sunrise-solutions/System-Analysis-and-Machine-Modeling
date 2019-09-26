eb716e146e44fb7a9071f4aaaed1ffed0107ca6e
npovalyaeva <iriejamaica@outlook.com>
2019-09-26 12:31:54 +0300

    Add ExponentialDistribution module (without bar graph)

--- Distributions/ExponentialDistribution.cs
+++ Distributions/ExponentialDistribution.cs
@@ -11,7 +11,28 @@ namespace Distributions
     {
         private void Button3_Click(object sender, EventArgs e)
         {
+            int m = Convert.ToInt32(exMTextBox.Text);
+            int coefficientA = Convert.ToInt32(exCoefficientATextBox.Text);
+            int R0 = Convert.ToInt32(exR0TextBox.Text);
 
+            Generator generator = new Generator(coefficientA, m, R0);
+            List<double> sequence = new List<double>();
+
+            double λ = double.Parse(exλTextBox.Text);
+            sequence = Distribution.Exponential(generator.GetNext().Take(N), λ);
+
+            CalculateExponentialDistributionEstimates(λ);
+        }
+
+        private void CalculateExponentialDistributionEstimates(double λ)
+        {
+            double Mx = 1 / λ;
+            double Dx = 1 / Math.Pow(λ, 2);
+            double γx = Math.Sqrt(Dx);
+
+            exMxValueLabel.Text = Math.Round(Mx, 5).ToString();
+            exDxValueLabel.Text = Math.Round(Dx, 4).ToString();
+            exγxValueLabel.Text = Math.Round(γx, 4).ToString();
         }
     }
 }
--- Distributions/MainForm.Designer.cs
+++ Distributions/MainForm.Designer.cs
@@ -28,9 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tabControlDistributions = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.unChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -61,6 +64,21 @@
             this.button5 = new System.Windows.Forms.Button();
             this.tabPage6 = new System.Windows.Forms.TabPage();
             this.button6 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.exR0Label = new System.Windows.Forms.Label();
+            this.exCoefficientALabel = new System.Windows.Forms.Label();
+            this.exMLabel = new System.Windows.Forms.Label();
+            this.exR0TextBox = new System.Windows.Forms.TextBox();
+            this.exCoefficientATextBox = new System.Windows.Forms.TextBox();
+            this.exMTextBox = new System.Windows.Forms.TextBox();
+            this.exλLabel = new System.Windows.Forms.Label();
+            this.exλTextBox = new System.Windows.Forms.TextBox();
+            this.exγxValueLabel = new System.Windows.Forms.Label();
+            this.exDxValueLabel = new System.Windows.Forms.Label();
+            this.exMxValueLabel = new System.Windows.Forms.Label();
+            this.exγxLabel = new System.Windows.Forms.Label();
+            this.exDxLabel = new System.Windows.Forms.Label();
+            this.exMxLabel = new System.Windows.Forms.Label();
             this.tabControlDistributions.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.unChart)).BeginInit();
@@ -69,6 +87,7 @@
             this.tabPage4.SuspendLayout();
             this.tabPage5.SuspendLayout();
             this.tabPage6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControlDistributions
@@ -118,16 +137,16 @@
             // 
             // unChart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.unChart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.unChart.Legends.Add(legend2);
+            chartArea4.Name = "ChartArea1";
+            this.unChart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.unChart.Legends.Add(legend4);
             this.unChart.Location = new System.Drawing.Point(219, 118);
             this.unChart.Name = "unChart";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.unChart.Series.Add(series2);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.unChart.Series.Add(series4);
             this.unChart.Size = new System.Drawing.Size(300, 188);
             this.unChart.TabIndex = 17;
             this.unChart.Text = "chart1";
@@ -299,6 +318,21 @@
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.exγxValueLabel);
+            this.tabPage3.Controls.Add(this.exDxValueLabel);
+            this.tabPage3.Controls.Add(this.exMxValueLabel);
+            this.tabPage3.Controls.Add(this.exγxLabel);
+            this.tabPage3.Controls.Add(this.exDxLabel);
+            this.tabPage3.Controls.Add(this.exMxLabel);
+            this.tabPage3.Controls.Add(this.exλLabel);
+            this.tabPage3.Controls.Add(this.exλTextBox);
+            this.tabPage3.Controls.Add(this.exR0Label);
+            this.tabPage3.Controls.Add(this.exCoefficientALabel);
+            this.tabPage3.Controls.Add(this.exMLabel);
+            this.tabPage3.Controls.Add(this.exR0TextBox);
+            this.tabPage3.Controls.Add(this.exCoefficientATextBox);
+            this.tabPage3.Controls.Add(this.exMTextBox);
+            this.tabPage3.Controls.Add(this.chart1);
             this.tabPage3.Controls.Add(this.button3);
             this.tabPage3.Location = new System.Drawing.Point(4, 30);
             this.tabPage3.Margin = new System.Windows.Forms.Padding(2);
@@ -385,6 +419,137 @@
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.Button6_Click);
             // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(223, 109);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(300, 188);
+            this.chart1.TabIndex = 18;
+            this.chart1.Text = "exChart";
+            // 
+            // exR0Label
+            // 
+            this.exR0Label.AutoSize = true;
+            this.exR0Label.Location = new System.Drawing.Point(544, 33);
+            this.exR0Label.Name = "exR0Label";
+            this.exR0Label.Size = new System.Drawing.Size(32, 21);
+            this.exR0Label.TabIndex = 24;
+            this.exR0Label.Text = "R0";
+            // 
+            // exCoefficientALabel
+            // 
+            this.exCoefficientALabel.AutoSize = true;
+            this.exCoefficientALabel.Location = new System.Drawing.Point(281, 33);
+            this.exCoefficientALabel.Name = "exCoefficientALabel";
+            this.exCoefficientALabel.Size = new System.Drawing.Size(18, 21);
+            this.exCoefficientALabel.TabIndex = 23;
+            this.exCoefficientALabel.Text = "a";
+            // 
+            // exMLabel
+            // 
+            this.exMLabel.AutoSize = true;
+            this.exMLabel.Location = new System.Drawing.Point(39, 33);
+            this.exMLabel.Name = "exMLabel";
+            this.exMLabel.Size = new System.Drawing.Size(24, 21);
+            this.exMLabel.TabIndex = 22;
+            this.exMLabel.Text = "m";
+            // 
+            // exR0TextBox
+            // 
+            this.exR0TextBox.Location = new System.Drawing.Point(582, 25);
+            this.exR0TextBox.Name = "exR0TextBox";
+            this.exR0TextBox.Size = new System.Drawing.Size(182, 29);
+            this.exR0TextBox.TabIndex = 21;
+            // 
+            // exCoefficientATextBox
+            // 
+            this.exCoefficientATextBox.Location = new System.Drawing.Point(305, 25);
+            this.exCoefficientATextBox.Name = "exCoefficientATextBox";
+            this.exCoefficientATextBox.Size = new System.Drawing.Size(221, 29);
+            this.exCoefficientATextBox.TabIndex = 20;
+            // 
+            // exMTextBox
+            // 
+            this.exMTextBox.Location = new System.Drawing.Point(69, 25);
+            this.exMTextBox.Name = "exMTextBox";
+            this.exMTextBox.Size = new System.Drawing.Size(161, 29);
+            this.exMTextBox.TabIndex = 19;
+            // 
+            // exλLabel
+            // 
+            this.exλLabel.AutoSize = true;
+            this.exλLabel.Location = new System.Drawing.Point(45, 94);
+            this.exλLabel.Name = "exλLabel";
+            this.exλLabel.Size = new System.Drawing.Size(19, 21);
+            this.exλLabel.TabIndex = 26;
+            this.exλLabel.Text = "λ";
+            // 
+            // exλTextBox
+            // 
+            this.exλTextBox.Location = new System.Drawing.Point(69, 86);
+            this.exλTextBox.Name = "exλTextBox";
+            this.exλTextBox.Size = new System.Drawing.Size(100, 29);
+            this.exλTextBox.TabIndex = 25;
+            // 
+            // exγxValueLabel
+            // 
+            this.exγxValueLabel.AutoSize = true;
+            this.exγxValueLabel.Location = new System.Drawing.Point(134, 248);
+            this.exγxValueLabel.Name = "exγxValueLabel";
+            this.exγxValueLabel.Size = new System.Drawing.Size(0, 21);
+            this.exγxValueLabel.TabIndex = 32;
+            // 
+            // exDxValueLabel
+            // 
+            this.exDxValueLabel.AutoSize = true;
+            this.exDxValueLabel.Location = new System.Drawing.Point(134, 203);
+            this.exDxValueLabel.Name = "exDxValueLabel";
+            this.exDxValueLabel.Size = new System.Drawing.Size(0, 21);
+            this.exDxValueLabel.TabIndex = 31;
+            // 
+            // exMxValueLabel
+            // 
+            this.exMxValueLabel.AutoSize = true;
+            this.exMxValueLabel.Location = new System.Drawing.Point(134, 155);
+            this.exMxValueLabel.Name = "exMxValueLabel";
+            this.exMxValueLabel.Size = new System.Drawing.Size(0, 21);
+            this.exMxValueLabel.TabIndex = 30;
+            // 
+            // exγxLabel
+            // 
+            this.exγxLabel.AutoSize = true;
+            this.exγxLabel.Location = new System.Drawing.Point(45, 248);
+            this.exγxLabel.Name = "exγxLabel";
+            this.exγxLabel.Size = new System.Drawing.Size(27, 21);
+            this.exγxLabel.TabIndex = 29;
+            this.exγxLabel.Text = "γx";
+            // 
+            // exDxLabel
+            // 
+            this.exDxLabel.AutoSize = true;
+            this.exDxLabel.Location = new System.Drawing.Point(45, 203);
+            this.exDxLabel.Name = "exDxLabel";
+            this.exDxLabel.Size = new System.Drawing.Size(32, 21);
+            this.exDxLabel.TabIndex = 28;
+            this.exDxLabel.Text = "Dx";
+            // 
+            // exMxLabel
+            // 
+            this.exMxLabel.AutoSize = true;
+            this.exMxLabel.Location = new System.Drawing.Point(45, 155);
+            this.exMxLabel.Name = "exMxLabel";
+            this.exMxLabel.Size = new System.Drawing.Size(35, 21);
+            this.exMxLabel.TabIndex = 27;
+            this.exMxLabel.Text = "Mx";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -401,9 +566,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.unChart)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.tabPage4.ResumeLayout(false);
             this.tabPage5.ResumeLayout(false);
             this.tabPage6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -440,6 +607,21 @@
         private System.Windows.Forms.Label unγxLabel;
         private System.Windows.Forms.Label unDxLabel;
         private System.Windows.Forms.Label unMxLabel;
+        private System.Windows.Forms.Label exR0Label;
+        private System.Windows.Forms.Label exCoefficientALabel;
+        private System.Windows.Forms.Label exMLabel;
+        private System.Windows.Forms.TextBox exR0TextBox;
+        private System.Windows.Forms.TextBox exCoefficientATextBox;
+        private System.Windows.Forms.TextBox exMTextBox;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label exγxValueLabel;
+        private System.Windows.Forms.Label exDxValueLabel;
+        private System.Windows.Forms.Label exMxValueLabel;
+        private System.Windows.Forms.Label exγxLabel;
+        private System.Windows.Forms.Label exDxLabel;
+        private System.Windows.Forms.Label exMxLabel;
+        private System.Windows.Forms.Label exλLabel;
+        private System.Windows.Forms.TextBox exλTextBox;
     }
 }
 
